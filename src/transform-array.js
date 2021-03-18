4aebcd63110111dbd5c792e98c731075adaed4f1
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-18 22:37:09 +0300

    feat: added transform-array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  if(Array.isArray(arr) === false) {
+    throw new Error();
+  }
+  let array = [];
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] === '--discard-next') {
+      i += 1;
+      continue;
+    }else if(arr[i] === '--discard-prev') {
+      if(arr[i - 2] !== '--discard-next'){
+        array.pop();
+      }
+      continue;
+    }else if(arr[i] === '--double-next'){
+      if(arr[i+1] !== undefined){
+      array.push(arr[i+1]);
+      }
+      continue;
+    }else if(arr[i] === '--double-prev') {
+      if(arr[i - 1] !== undefined && arr[i - 2] !== '--discard-next'){
+        array.push(arr[i-1]);
+      }
+      continue;
+    } 
+    array.push(arr[i])
+  }
+  return array;
+}  
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
