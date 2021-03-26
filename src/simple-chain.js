ecd5a6e754fcfa82b6a88baf4f682cc3f427b019
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-26 18:32:54 +0300

    feat: added chain  function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain : [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push('( ' + value + ' )');
+    return this;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position < 1 || position > this.chain.length) {
+      this.chain = [];
+      throw new Error();
+    } else {
+    this.chain.splice(position - 1, 1);
+    return this;
+    }
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let arr = [];
+    let arr1 = arr.concat(this.chain)
+    this.chain = [];
+    return arr1.join("~~");
   }
 };
 
