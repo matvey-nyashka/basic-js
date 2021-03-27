faf346b439ad9c8dcc7dccf9c741d3b4cf5d8d0d
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-03-27 13:20:35 +0300

    feat: added repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let adit = [];
+  let addition = (typeof options.addition != "undefined") ? String(options.addition) : "";
+  if(options.additionRepeatTimes){
+    for (let i =0; i < options.additionRepeatTimes; i++){
+      adit.push(addition)
+    }
+  }else {
+    adit.push(String(addition))
+  }
+  let additionSep = (options.additionSeparator) ? options.additionSeparator : '|'
+  let sep = adit.join(additionSep);
+  let arr = [];
+  if(options.repeatTimes){
+    for (let i = 0; i < options.repeatTimes; i++){
+    arr.push(String(str) + sep)
+    }
+  }else {
+    arr.push(String(str) + sep)
+  }  
+  let adSep = (options.separator) ? options.separator : '+'
+  return arr.join(adSep)
+
 };
   
