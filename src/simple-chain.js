33155d1a52b21e68ac4b67b04f6869f314c449e6
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-08 17:21:50 +0300

    feat: added solution simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  
+    chain : [],
+  
+    getLength() {
+      return this.chain.length;
+    },
+  
+    addLink(value) {
+      this.chain.push('( ' + value + ' )');
+      return this;
+    },
+  
+    removeLink(position) {
+      if (position < 1 || position > this.chain.length) {
+        this.chain = [];
+        throw new Error();
+      } else {
+      this.chain.splice(position - 1, 1);
+      return this;
+      }
+    },
+  
+    reverseChain() {
+      this.chain.reverse();
+      return this;
+    },
+  
+    finishChain() {
+      let arr = [];
+      let arr1 = arr.concat(this.chain)
+      this.chain = [];
+      return arr1.join("~~");
+    }
+  
 };
