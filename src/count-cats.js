ac06f4d48a5546d3f96c54e986098d879f01c6ed
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-08 14:58:08 +0300

    feat: added solution count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let arr = matrix.flat().filter(value => value == '^^')
+  return arr.length;
 }
