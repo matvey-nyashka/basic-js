4f3aa62c0364bf869926a87c125e09c7237d07f9
matvey-nyashka <matvej.gnedin.10@mail.ru>
2021-09-09 16:06:51 +0300

    feat: added solution st-file-names

--- src/st-file-names.js
+++ src/st-file-names.js
@@ -15,7 +15,29 @@ import { NotImplementedError } from '../extensions/index.js';
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-export default function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function renameFiles(names) {
+  const arr = [];
+  let index;
+  let flag;
+  let count;
+  let test;
+  for (let i = 0; i < names.length; i++) {
+    count = 1;
+    if (arr.includes(names[i])) {
+      flag = false;
+      index = arr.indexOf(names[i]);
+    } else {
+      arr.push(names[i]);
+    }
+    while (flag === false) {
+      test = `${arr[index]}(${count})`;
+      if (arr.includes(test)) {
+        count++;
+      } else {
+        arr.push(test);
+        flag = true;
+      }
+    }
+  }
+  return arr;
 }
